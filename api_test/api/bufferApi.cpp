4e80d24d72165ccc9c621e6293f7ae86b9bd7415
GongBaiRong <gby@edu.nuc.email>
2026-03-07 09:39:33 +0800

    分离log模块测试代码

--- api_test/api/bufferApi.cpp
+++ api_test/api/bufferApi.cpp
@@ -1,5 +1,4 @@
 #include "../../include/buffer.h"
-#include "../../include/Log.h"
 using namespace std;
 void testBufferBasic()
 {
@@ -184,24 +183,6 @@ void testBufferContinuousLargeInput()
     std::cout << "连续输入大量数据测试通过!" << std::endl;
 }
 
-// 测试日志输出
-void testLog()
-{
-    LOG("INFO", "This is an info message.");
-    LOG("ERROR", "This is an error message.");
-    Buffer buf(8);
-    std::string largeData(1000, 'x');
-    for (int i = 0; i < 10; ++i)
-    {
-        bool writeResult = buf.Write(largeData);
-        assert(writeResult);
-        LOG("INFO", "第 " << i + 1 << " 次写入完成,当前缓冲区大小: " << buf.GetSize());
-    }
-    std::string out = buf.Read(10000);
-    assert(out == std::string(10000, 'x'));
-    LOG("INFO", "连续输入大量数据测试通过!");
-}
-
 int main()
 {
     testBufferBasic();
@@ -211,6 +192,5 @@ int main()
     testBufferError();
     testBufferWriteBuffer();
     testBufferContinuousLargeInput();
-    testLog();
     return 0;
 }

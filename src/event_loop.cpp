7cfc66e4f2348196f98af0bb40552ac7e823cf18
GongBaiRong <gby@edu.nuc.email>
2026-03-13 19:56:23 +0800

    修改EventLoop类Start函数

--- src/event_loop.cpp
+++ src/event_loop.cpp
@@ -8,23 +8,21 @@ void EventLoop::_RunAllTasks()
     {
         // 获取锁,并且自动管理
         std::unique_lock<std::mutex> lock(this->_queue_mutex);
-        tasks.swap(this->_tasks_queue); // 交换任务队列,避免长时间持有锁
+        tasks.swap(this->_tasks_queue);  // 交换任务队列,避免长时间持有锁
     }
 
     for (const Action &task : tasks)
     {
-        task(); // 执行任务
+        task();  // 执行任务
     }
 }
 
-EventLoop::~EventLoop()
-{
-    delete this->_timer;
-}
+EventLoop::~EventLoop() { delete this->_timer; }
 
-EventLoop::EventLoop() : _poller(),
-                         _efd_channel(this, std::move(Socket(eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK)))),
-                         _thread_id(std::this_thread::get_id())
+EventLoop::EventLoop()
+    : _poller(),
+      _efd_channel(this, std::move(Socket(eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK)))),
+      _thread_id(std::this_thread::get_id())
 {
     if (this->_efd_channel.GetSocket().GetSocketFd() < 0)
     {
@@ -32,14 +30,15 @@ EventLoop::EventLoop() : _poller(),
         exit(EXIT_FAILURE);
     }
 
-    this->_timer = new Timer(this); // 创建定时器对象,用于管理定时器任务
+    this->_timer = new Timer(this);  // 创建定时器对象,用于管理定时器任务
 
     // 当事件fd可读时说明有任务需要处理
     this->_efd_channel.readAction = [this]()
     {
         uint64_t one;
         // ssize_t size = this->_efd_channel.GetSocket().Recv(&one, sizeof(one)); // 读取事件fd,清除可读事件
-        ssize_t size = read(this->_efd_channel.GetSocket().GetSocketFd(), &one, sizeof(one)); // 直接使用read读取事件fd,清除可读事件
+        ssize_t size = read(this->_efd_channel.GetSocket().GetSocketFd(), &one,
+                            sizeof(one));  // 直接使用read读取事件fd,清除可读事件
         if (size < 0)
         {
             LOG(ERROR, "Failed to read from eventfd");
@@ -48,38 +47,39 @@ EventLoop::EventLoop() : _poller(),
         // 读取成功,计数清零
     };
 
-    this->_efd_channel.EnableRead(); // 启动事件fd的可读事件监控,当可读时说明有任务需要处理
+    this->_efd_channel.EnableRead();  // 启动事件fd的可读事件监控,当可读时说明有任务需要处理
 }
 
 void EventLoop::Start()
 {
-    // 事件监控
-    std::vector<Channel *> activeChannels = this->_poller.Poll(-1);
-    for (Channel *channel : activeChannels)
+    while (true)
     {
-        channel->HandleEvent(); // 就绪事件处理
+        // 事件监控
+        std::vector<Channel *> activeChannels = this->_poller.Poll(-1);
+        for (Channel *channel : activeChannels)
+        {
+            channel->HandleEvent();  // 就绪事件处理
+        }
+        // 执行任务
+        this->_RunAllTasks();
     }
-    // 执行任务
-    this->_RunAllTasks();
 }
 
 // 判断当前线程是否是事件循环所在的线程
-bool EventLoop::InLoop()
-{
-    return this->_thread_id == std::this_thread::get_id();
-}
+bool EventLoop::InLoop() { return this->_thread_id == std::this_thread::get_id(); }
 
 // 将不在EventLoop线程的任务压入队列中
 void EventLoop::AddTask(const Action &task)
 {
     {
         std::unique_lock<std::mutex> lock(this->_queue_mutex);
-        this->_tasks_queue.push_back(task); // 将任务添加到队列中
+        this->_tasks_queue.push_back(task);  // 将任务添加到队列中
     }
     // 唤醒可能因为没有事件就绪而导致的epoll阻塞,否则Start->RunAllTasks可能无法及时执行
     uint64_t one = 1;
     // ssize_t size = this->_efd_channel.GetSocket().Send(&one, sizeof(one)); // 写入事件fd,触发可读事件
-    ssize_t size = write(this->_efd_channel.GetSocket().GetSocketFd(), &one, sizeof(one)); // 直接使用write写入事件fd,触发可读事件
+    ssize_t size =
+        write(this->_efd_channel.GetSocket().GetSocketFd(), &one, sizeof(one));  // 直接使用write写入事件fd,触发可读事件
     if (size < 0)
     {
         LOG(ERROR, "Failed to write to eventfd");
@@ -96,7 +96,7 @@ void EventLoop::RunTask(const Action &task)
         return;
     }
     // 当前任务不在EventLoop线程上
-    this->AddTask(task); // 将任务添加到队列中
+    this->AddTask(task);  // 将任务添加到队列中
 }
 
 // 在Poller中移除channel事件监控
@@ -106,7 +106,10 @@ void EventLoop::AddChannel(Channel *channel) { this->_poller.AddChannel(channel)
 void EventLoop::RemoveChannel(Channel *channel) { this->_poller.RemoveChannel(channel); }
 
 // 添加定时器任务
-void EventLoop::AddTimerTask(uint64_t id, uint64_t expireTime, Action action) { this->_timer->Add(id, expireTime, action); }
+void EventLoop::AddTimerTask(uint64_t id, uint64_t expireTime, Action action)
+{
+    this->_timer->Add(id, expireTime, action);
+}
 
 // 刷新定时器任务的到期时间
 void EventLoop::RefreshTimerTask(uint64_t id, uint64_t newExpireTime) { this->_timer->Refresh(id, newExpireTime); }

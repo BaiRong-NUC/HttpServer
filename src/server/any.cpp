823536eb5c3bbf4412c661c7758842c5a0c060ba
GongBaiRong <gby@edu.nuc.email>
2026-03-22 19:12:12 +0800

    将模板实现移入 any.h:并从 any.cpp: 中移除这些模板实现,修复了编译错误

--- src/server/any.cpp
+++ src/server/any.cpp
@@ -1,20 +1,7 @@
 #include "server/any.h"
-
 Any::Base::~Base() {}
 
-template <class T>
-Any::Placeholder<T>::Placeholder(T const &value) : value(value) {}
-
-template <class T>
-std::type_info const &Any::Placeholder<T>::GetType() const { return typeid(T); }
-
-template <class T>
-Any::Base *Any::Placeholder<T>::Clone() const { return new Placeholder(value); }
-
 Any::Any() : _content(nullptr) {}
-
-template <class T>
-Any::Any(T const &value) : _content(new Placeholder<T>(value)) {}
 
 Any::Any(Any const &other) : _content(other._content ? other._content->Clone() : nullptr) {}
 
@@ -23,21 +10,6 @@ Any::~Any() { delete _content; }
 Any &Any::Swap(Any &other)
 {
     std::swap(_content, other._content);
-    return *this;
-}
-
-template <class T>
-T *Any::Get()
-{
-    if (_content && _content->GetType() == typeid(T))
-        return &static_cast<Placeholder<T> *>(_content)->value;
-    return nullptr;
-}
-
-template <class T>
-Any &Any::operator=(T const &value)
-{
-    Any(value).Swap(*this);
     return *this;
 }
 

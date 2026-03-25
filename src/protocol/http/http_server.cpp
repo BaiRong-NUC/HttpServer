ab9fc81005897a996503b546760dd9a9ccb20fdd
GongBaiRong <gbr@edu.nuc.email>
2026-03-25 20:08:08 +0800

    出错直接清除缓冲区立刻关闭连接

--- src/protocol/http/http_server.cpp
+++ src/protocol/http/http_server.cpp
@@ -62,6 +62,7 @@ void HttpServer::_OnMessage(const PtrConnection &conn, Buffer *buffer)
             this->SendResponse(conn, context->GetRequest(), error_response);
             // 切斷連接
             context->Reset();  // 重置上下文状态,以防止conn关闭因为缓冲区不为空,进入这个函数导致死循环
+            buffer->Clear();   // 缓冲区剩下的内容不处理了
             conn->Close();
             return;
         }

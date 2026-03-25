25802ffccb2a9ac54c294e2392f74e6de748d55d
GongBaiRong <gbr@edu.nuc.email>
2026-03-25 17:58:27 +0800

    修改客户端测试程序

--- test/client.cpp
+++ test/client.cpp
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
 {
     Socket clientSocket;
     clientSocket.CreateClient("127.0.0.1", 8085);
+    int times = 0;
     std::string req = "GET / HTTP/1.1\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n";
     while (true)
     {
@@ -15,7 +16,7 @@ int main(int argc, char const *argv[])
             break;
         }
         std::cout << "Sent request to server: " << req;
-        char buffer[1024];
+        char buffer[1024] = {0};
         ret = clientSocket.Recv(buffer, sizeof(buffer) - 1);
         if (ret < 0)
         {
@@ -23,6 +24,7 @@ int main(int argc, char const *argv[])
             break;
         }
         sleep(3);
+        std::cout << "Send Times: " << ++times << " Run Time:" << times * 3 << "s\n===========" << std::endl;
     }
     clientSocket.Close();
     return 0;
